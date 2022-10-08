27eb223fafa32861c63fc8274f35f59c94522c12
Draav Jain <draavjain2001@gmail.com>
2022-10-08 13:26:33 +0530

    initial commit

---
+++ src/SearchForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+
+const SearchForm = () => {
+  const { query, handleSearch, toggleTheme } = useGlobalContext();
+
+  return (
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+      <div>
+        <h2>search hacker news</h2>
+        <input
+          type="text"
+          className="form-input"
+          value={query}
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+      </div>
+      <div
+        // style={{
+        //   display: "grid",
+        //   placeItems: "center",
+        // }}
+      >
+        <button className="btn" onClick={toggleTheme}>
+          Toggle
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SearchForm;
---
+++ src/context.js
@@ -0,0 +1,93 @@
+import React, { useContext, useEffect, useReducer, useState } from "react";
+
+import {
+  SET_LOADING,
+  SET_STORIES,
+  REMOVE_STORY,
+  HANDLE_PAGE,
+  HANDLE_SEARCH,
+} from "./actions";
+import reducer from "./reducer";
+
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?";
+
+const initialState = {
+  isLoading: true,
+  hits: [],
+  query: "react",
+  page: 0,
+  nbPages: 0,
+};
+
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+};
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchStories = async (url) => {
+    dispatch({ type: SET_LOADING });
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      dispatch({
+        type: SET_STORIES,
+        payload: { hits: data.hits, nbPages: data.nbPages },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const [theme, setTheme] = useState(getStorageTheme);
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const removeStory = (id) => {
+    dispatch({ type: REMOVE_STORY, payload: id });
+  };
+
+  const handleSearch = (query) => {
+    dispatch({ type: HANDLE_SEARCH, payload: query });
+  };
+
+  const handlePage = (value) => {
+    dispatch({ type: HANDLE_PAGE, payload: value });
+  };
+
+  useEffect(() => {
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
+  }, [state.query, state.page]);
+
+  return (
+    <AppContext.Provider
+      value={{ ...state, removeStory, handleSearch, handlePage, toggleTheme }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
